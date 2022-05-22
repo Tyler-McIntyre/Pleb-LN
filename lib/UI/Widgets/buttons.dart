2f1d3ffd18832a6377a70e5d0936081f7854d43c
Tyler-McIntyre <tmcintyre802@gmail.com>
2022-05-22 14:35:11 -0400

    Remove avator glow from startup screen

--- lib/UI/Widgets/buttons.dart
+++ lib/UI/Widgets/buttons.dart
@@ -1,6 +1,4 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../util/app_assets.dart';
 import '../node_config_screen.dart';
 import '../../util/app_colors.dart';
@@ -10,40 +8,32 @@ class LinkNodeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AvatarGlow(
-      endRadius: 180,
-      showTwoGlows: false,
-      glowColor: AppColors.white,
-      shape: BoxShape.circle,
-      child: SizedBox(
-        height: 120,
-        width: 120,
-        child: TextButton(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                FontAwesomeIcons.raspberryPi,
-                color: AppColors.white,
-                size: 100,
-              ),
-            ],
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const NodeConfigScreen(),
-              ),
-            );
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(AppColors.transparent),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(72.0),
-                side: const BorderSide(color: AppColors.transparent),
-              ),
+    return SizedBox(
+      height: 300,
+      width: 300,
+      child: TextButton(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/Pleb-logos_round.png',
+            ),
+          ],
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NodeConfigScreen(),
+            ),
+          );
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(AppColors.transparent),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(72.0),
+              side: const BorderSide(color: AppColors.transparent),
             ),
           ),
         ),
