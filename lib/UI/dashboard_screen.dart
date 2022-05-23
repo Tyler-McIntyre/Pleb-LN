f0856c72699f84dab58687a2b35c0c1bcd584244
Tyler-McIntyre <tmcintyre802@gmail.com>
2022-05-23 06:02:30 -0400

    Fix late init error on isConfigured

--- lib/UI/dashboard_screen.dart
+++ lib/UI/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../database/secure_storage.dart';
+import '../util/app_colors.dart';
 import 'Widgets/activities.dart';
 import 'Widgets/dashboard_header.dart';
 
@@ -34,13 +35,50 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          DashboardHeader(nodeIsConfigured: nodeIsConfigured),
-          Expanded(
-            child: Activities(nodeIsConfigured: nodeIsConfigured),
-          )
-        ],
+      body: FutureBuilder(
+        future: _nodeIsConfigured(),
+        builder: (context, snapshot) {
+          late Widget child;
+          if (snapshot.hasData) {
+            child = Column(
+              children: [
+                DashboardHeader(nodeIsConfigured: nodeIsConfigured),
+                Expanded(
+                  child: Activities(nodeIsConfigured: nodeIsConfigured),
+                )
+              ],
+            );
+          } else if (snapshot.hasError) {
+            child = Column(children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: TextStyle(color: AppColors.red),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ]);
+          } else {
+            child = SizedBox(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+            child: child,
+          );
+        },
       ),
     );
   }
